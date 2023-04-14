1bfcc80a4effccc928e0d22d01c1efdf7034c5ae
Matheus Valero <matheusvalero99@gmail.com>
2023-04-14 12:39:00 -0300

    Update

---
+++ ExerciceFrançais/src/Exercice.java
@@ -0,0 +1,14 @@
+public class Exercice {
+    public static void main(String[] args) {
+        System.out.println("TEXT 1\n"+
+        "Bonjour,\nJe m'appelle Matheus. Je suis\n"+
+        "brésilien e j'habite à Brasília. Je suis\n"+
+        "réceptionniste dans un hôtel. Je travaille\n"+
+        "beaucoup. Je parle portugais, français, italien\n"+
+        "et allemand. J'ai 23 ans et je suis marié.\n"+
+        "Au revoir,\nMatheus\n\n");
+
+        System.out.println("Activité\n"+
+        "Lisez le texte de présentation de Matheus. Ensuite");
+    }
+}
